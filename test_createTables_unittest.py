37444f6220aea31b90e5f40268495de5f5f40811
trutik <49213947+trutik@users.noreply.github.com>
2020-09-19 14:25:11 +0100

    Update test_createTables_unittest.py
    Set up code was being repeated in every test, placed code under setUpClass to initialise variables only once.

--- test_createTables_unittest.py
+++ test_createTables_unittest.py
@@ -1,63 +1,51 @@
-import unittest
-from vigenere import *
+from unittest import TestCase, main
+from vigenere import createTables
 
 
-class TestCreateTables(unittest.TestCase):
+class TestCreateTables(TestCase):
 
-    def setUp(self):
-        pass
+    @classmethod
+    def setUpClass(cls):
+        """ Using setUpClass to generate data sets only once, as they will be re-used across all tests. """
+        tables = createTables(list('TEST'))
+        cls.encryption_table = tables[0]
+        cls.decryption_table = tables[1]
+
     #Test each corner of the 'tabula recta' and the center, is mapping as expected?
-    def test_createtablesENC_top_left(self):
-        tables = createTables(list('TEST'))
-        table = tables[0]
-        self.assertEqual( table['AA'], 'A')
 
-    def test_createtablesENC_top_right(self):
-        tables = createTables(list('TEST'))
-        table = tables[0]
-        self.assertEqual( table['AZ'], 'Z')
+    #Test encryption table.
 
-    def test_createtablesENC_center(self):
-        tables = createTables(list('TEST'))
-        table = tables[0]
-        self.assertEqual( table['LM'], 'X')
+    def test_createtables_encryption_top_left(self):
+        self.assertEqual( self.encryption_table['AA'], 'A')
 
-    def test_createtablesENC_bottom_left(self):
-        tables = createTables(list('TEST'))
-        table = tables[0]
-        self.assertEqual( table['ZA'], 'Z')
+    def test_createtables_encryption_top_right(self):
+        self.assertEqual( self.encryption_table['AZ'], 'Z')
 
-    def test_createtablesENC_bottom_right(self):
-        tables = createTables(list('TEST'))
-        table = tables[0]
-        self.assertEqual( table['ZZ'], 'Y')
+    def test_createtables_encryption_center(self):
+        self.assertEqual( self.encryption_table['LM'], 'X')
 
-    ##############################
-    #Test decryption table now
-    def test_createtablesDEC_top_left(self):
-        tables = createTables(list('TEST'))
-        table = tables[1]
-        self.assertEqual( table['AA'], 'A')
+    def test_createtables_encryption_bottom_left(self):
+        self.assertEqual( self.encryption_table['ZA'], 'Z')
 
-    def test_createtablesDEC_top_right(self):
-        tables = createTables(list('TEST'))
-        table = tables[1]
-        self.assertEqual( table['AZ'], 'Z')
+    def test_createtables_encryption_bottom_right(self):
+        self.assertEqual( self.encryption_table['ZZ'], 'Y')
 
-    def test_createtablesDEC_center(self):
-        tables = createTables(list('TEST'))
-        table = tables[1]
-        self.assertEqual( table['LX'], 'M')
+    #Testing decryption table.
 
-    def test_createtablesDEC_bottom_left(self):
-        tables = createTables(list('TEST'))
-        table = tables[1]
-        self.assertEqual( table['ZZ'], 'A')
+    def test_createtables_decryption_top_left(self):
+        self.assertEqual( self.decryption_table['AA'], 'A')
 
-    def test_createtablesDEC_bottom_right(self):
-        tables = createTables(list('TEST'))
-        table = tables[1]
-        self.assertEqual( table['ZY'], 'Z')
+    def test_createtables_decryption_top_right(self):
+        self.assertEqual( self.decryption_table['AZ'], 'Z')
+
+    def test_createtables_decryption_center(self):
+        self.assertEqual( self.decryption_table['LX'], 'M')
+
+    def test_createtables_decryption_bottom_left(self):
+        self.assertEqual( self.decryption_table['ZZ'], 'A')
+
+    def test_createtables_decryption_bottom_right(self):
+        self.assertEqual( self.decryption_table['ZY'], 'Z')
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
